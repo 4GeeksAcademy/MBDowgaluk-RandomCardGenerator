57a1d0c600e139e8391e030c04b4884fa798c4c3
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-10-22 18:48:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+
+function generateRandomNumber() {
+  let numbersCards = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * numbersCards.length);
+  return numbersCards[indexNumber];
+}
+
+function generateRandomSuit() {
+  let suit = ["diams", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+}
